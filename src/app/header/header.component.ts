1d0d124dd8fc4e1338ec9b0178c37ec9772fa201
adrijana.jovicic@gmail.com <adrijana.jovicic@gmail.com>
2021-04-05 16:02:49 +0200

    add to cart

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ShopService } from '../shared/services/shop.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  subscription: any;
+  private _completed = new Subject();
+  cartNumber: number = 0;
+
+  constructor(
+    private _shopService: ShopService
+
+  ) { }
 
   ngOnInit() {
+    this.subscription = this._shopService.getCartNumberChangeEmitter()
+      .pipe(takeUntil(this._completed))
+      .subscribe(item => this.cartNumberChanged(item));
+  }
+
+  cartNumberChanged(numb: any) {
+    this.cartNumber = numb;
+  }
+
+  ngOnDestroy() {
+    this._completed.next();
+    this._completed.complete();
   }
 
 }
